8ec815de24a744444bc0468bcfa48d7c9d5eaa8f
Leandro672 <leandro.gomes@sou.inteli.edu.br>
2023-05-15 05:08:37 -0300

    Ponderada semana 4
    Entrega da ponderada da semana 4

---
+++ SEMANA_04/02_AUT_EST_ENTREGA/app.js
@@ -0,0 +1,69 @@
+    const express = require('express'); 
+    const app = express();
+
+    const hostname = '127.0.0.1';
+    const port = 3000;
+    const sqlite3 = require('sqlite3').verbose();
+    const DBPATH = 'curriculo.db'; //use o nome que você achar melhor para o banco de dados
+
+    app.use(express.json());
+
+    app.get('/formacao', (req, res) => {
+        res.statusCode = 200;
+        res.setHeader('Access-Control-Allow-Origin', '*'); 
+        var db = new sqlite3.Database(DBPATH); // Abre o banco
+        var sql = 'SELECT titulo, descricao, data_inicio, data_fim FROM formacao ORDER BY data_fim DESC';
+        db.all(sql, [],  (err, rows ) => {
+            if (err) {
+                throw err;
+            }
+            res.json(rows);
+        });
+        db.close(); // Fecha o banco
+    });
+
+    app.get('/dadospessoais', (req, res) => {
+        res.statusCode = 200;
+        res.setHeader('Access-Control-Allow-Origin', '*'); 
+        var db = new sqlite3.Database(DBPATH); // Abre o banco
+        var sql = 'SELECT nome, cargo, endereco, telefone, email, descricao FROM dadospessoais ORDER BY nome COLLATE NOCASE';
+        db.all(sql, [],  (err, rows ) => {
+            if (err) {
+                throw err;
+            }
+            res.json(rows);
+        });
+        db.close(); // Fecha o banco
+    });
+
+    app.get('/experiencia', (req, res) => {
+        res.statusCode = 200;
+        res.setHeader('Access-Control-Allow-Origin', '*'); 
+        var db = new sqlite3.Database(DBPATH); // Abre o banco
+        var sql = 'SELECT nome_empresa, data_inicio, data_fim, cargo, descricao FROM experiencia ORDER BY data_fim DESC';
+        db.all(sql, [],  (err, rows ) => {
+            if (err) {
+                throw err;
+            }
+            res.json(rows);
+        });
+        db.close(); // Fecha o banco
+    });
+
+    app.get('/realizacoes', (req, res) => {
+        res.statusCode = 200;
+        res.setHeader('Access-Control-Allow-Origin', '*'); 
+        var db = new sqlite3.Database(DBPATH); // Abre o banco
+        var sql = 'SELECT titulo, data, descricao FROM realizacoes ORDER BY data DESC';
+        db.all(sql, [],  (err, rows ) => {
+            if (err) {
+                throw err;
+            }
+            res.json(rows);
+        });
+        db.close(); // Fecha o banco
+    });
+    
+    app.listen(port, hostname, () => {
+        console.log(`Servidor rodando em http://${hostname}:${port}/`);
+        });
